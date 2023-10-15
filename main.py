3919d88ed47707b520c47f0ec8c42bba3985f6d2
Stanley Ndagi <ndagis@gmail.com>
2023-10-15 14:37:57 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def sum_negative(num1, num2):
+    if num1 < 0 and num2 < 0:
+        return num1 + num2
+    else:
+        return None
+
+def sum_positive(num1, num2):
+    if num1 > 0 and num2 > 0:
+        return num1 + num2
+    else:
+        return None
