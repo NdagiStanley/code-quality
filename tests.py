55a99f932b5be6c8d2ce1ec6553f2435fba460cd
Stanley Ndagi <ndagis@gmail.com>
2023-10-15 15:01:58 +0300

    test: add tests - for test coverage

--- tests.py
+++ tests.py
@@ -3,10 +3,16 @@ from main import sum_negative, sum_positive
 
 class SumTests(unittest.TestCase):
 
-    def test_sum(self):
+    def test_sum_negative_ok(self):
         self.assertEqual (sum_negative(-5, -5), -10)
-        self.assertEqual (sum_negative(5, 2), None)
+        self.assertEqual (sum_negative(5, 5), None)
+        self.assertEqual (sum_negative(5, -2), None)
+        self.assertEqual (sum_negative(-5, 2), None)
+        self.assertEqual (sum_negative(0, 0), None)
 
     def test_sum_positive_ok(self):
         self.assertEqual (sum_positive(2, 2), 4)
-        self.assertEqual (sum_positive(-5, -2), None)
+        self.assertEqual (sum_positive(-2, -2), None)
+        self.assertEqual (sum_positive(2, -2), None)
+        self.assertEqual (sum_positive(-2, 2), None)
+        self.assertEqual (sum_positive(0, 0), None)
